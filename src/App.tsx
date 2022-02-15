7c04dbb38aa0c8850358fc2c1ca042171d1d028d
drmrhdt <drmrhdt@gmail.com>
2022-02-15 17:22:46 +0300

    feat: add material ui library, add tabs component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,61 @@
-import React from 'react';
-import logo from './assets/logo.svg';
-import './App.scss';
+import React from "react";
+import logo from "./assets/logo.svg";
+import "./App.scss";
+
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+import TabPanel from "./components/TabPanel";
 
 function App() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: any, newValue: any) => {
+    setValue(newValue);
+  };
+
+  const a11yProps = (index: any) => {
+    return {
+      id: `simple-tab-${index}`,
+      "aria-controls": `simple-tabpanel-${index}`,
+    };
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <div className="header__container">
-        <a href="/">
-        <img src={logo} className="App-logo" alt="logo" />
-        </a>
+          <a href="/">
+            <img src={logo} className="App-logo" alt="logo" />
+          </a>
         </div>
       </header>
       <div className="banner">
-      <div className="banner__container">
-      <h1 className="banner__logo">Order Food</h1>
-        <b>
-        From 175 Restaurants
-        </b>
+        <div className="banner__container">
+          <h1 className="banner__logo">Order Food</h1>
+          <b>From 175 Restaurants</b>
+        </div>
       </div>
-      </div>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+        >
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
     </div>
   );
 }
