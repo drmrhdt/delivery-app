267f25e5a8243591cc57025fda06e10191b8c14e
drmrhdt <drmrhdt@gmail.com>
2022-02-21 13:31:16 +0300

    refactor: separate card component

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,8 @@ import TabPanel from "./components/TabPanel";
 import logo from "./assets/logo.svg";
 import "./App.scss";
 
-import { useTheme } from "@mui/material/styles";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import Rating from "@mui/material/Rating";
+import RestaurantCard from "./components/RestaurantCard";
 
 function App() {
   const [restaurants, setRestaurants] = React.useState([]);
@@ -88,45 +83,7 @@ function App() {
             {restaurants.map((restaurant: any) => {
               if (restaurant.cuisines.includes(cuisines[value])) {
                 return (
-                  <Card sx={{ display: "flex",  marginBottom: '15px' }}>
-                    <Box sx={{ display: "flex", flexDirection: "column" }}>
-                      <CardContent sx={{ flex: "1 0 auto" }}>
-                        <Box sx={{ display: "flex", flexDirection: "row" }}>
-                          <CardMedia
-                            component="img"
-                            sx={{ width: 151 }}
-                            image="https://www.globalblue.com/destinations/russia/moscow/article705149.ece/BINARY/yoko_restaurant_teaser.jpg"
-                            alt="Live from space album cover"
-                          />
-                          <Box
-                            sx={{ display: "flex", flexDirection: "column", marginLeft: '10px' }}
-                          >
-                            <Typography component="div" variant="h5">
-                              {restaurant.name}
-                            </Typography>
-                            <Typography
-                              variant="subtitle1"
-                              color="text.secondary"
-                              component="div"
-                            >
-                              {restaurant.cuisines.map(
-                                (cuisine: any, i: number) =>
-                                  cuisine +
-                                  (i === restaurant.cuisines.length - 1
-                                    ? ""
-                                    : ", ")
-                              )}
-                            </Typography>
-                            <Rating
-                              name="read-only"
-                              value={restaurant.averageRating}
-                              readOnly
-                            />
-                          </Box>
-                        </Box>
-                      </CardContent>
-                    </Box>
-                  </Card>
+                  <RestaurantCard restaurant={restaurant}></RestaurantCard>
                 );
               }
             })}
---
+++ src/components/RestaurantCard.tsx
@@ -0,0 +1,54 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import Rating from "@mui/material/Rating";
+import Box from "@mui/material/Box";
+
+
+function RestaurantCard(props: any) {
+  return (
+    <Card sx={{ display: "flex", marginBottom: "15px" }}>
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <CardContent sx={{ flex: "1 0 auto" }}>
+          <Box sx={{ display: "flex", flexDirection: "row" }}>
+            <CardMedia
+              component="img"
+              sx={{ width: 151 }}
+              image="https://www.globalblue.com/destinations/russia/moscow/article705149.ece/BINARY/yoko_restaurant_teaser.jpg"
+              alt="Live from space album cover"
+            />
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                marginLeft: "10px",
+              }}
+            >
+              <Typography component="div" variant="h5">
+                {props.restaurant.name}
+              </Typography>
+              <Typography
+                variant="subtitle1"
+                color="text.secondary"
+                component="div"
+              >
+                {props.restaurant.cuisines.map(
+                  (cuisine: any, i: number) =>
+                    cuisine + (i === props.restaurant.cuisines.length - 1 ? "" : ", ")
+                )}
+              </Typography>
+              <Rating
+                name="read-only"
+                value={props.restaurant.averageRating}
+                readOnly
+              />
+            </Box>
+          </Box>
+        </CardContent>
+      </Box>
+    </Card>
+  );
+}
+
+export default RestaurantCard;
